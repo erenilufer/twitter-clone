4ab8706d333d92b440ad1a11bedd0877a6d8cc38
erennilufer <eren.nilufer@outlook.com>
2022-09-19 00:08:09 +0300

    added multer

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./src/routes/authRoutes.js";
 import tweetRoutes from "./src/routes/tweetRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import multer from "multer";
 
 const app = express();
 const port = 3001;
@@ -18,9 +20,24 @@ mongoose
   })
   .then(() => console.log("connected to db"));
 
+// Image upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "src/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "image.jpeg"); // Image name will be setted
+  },
+});
+const upload = multer({ storage: storage });
+app.post("/upload/image", upload.single("file"), (req, res) => {
+  res.status(200).json({ message: "success" });
+});
+
 // Routes
 app.use(authRoutes);
 app.use(tweetRoutes);
+app.use(userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server Running!");
