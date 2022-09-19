c871651b5cf9eb345d763caeecf46f6069b0d6c9
erennilufer <eren.nilufer@outlook.com>
2022-09-20 02:40:54 +0300

    css fix

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,16 +1,42 @@
-import React from "react";
+import FeedHeader from "../../components/FeedHeader/FeedHeader";
 import SideNavigation from "../../components/SideNavigation/SideNavigation";
+import Tweet from "../../components/Tweet/Tweet";
 
 interface Props {}
 
 const Home = (props: Props) => {
   return (
-    <div className="h-screen  bg-[#1E2733]">
-      <div className="flex justify-between mx-auto max-w-5xl includes 3 component">
-        <SideNavigation/>
-        <div>Feed</div>
-        <div>RightBar</div>
-
+    <div className="æ">
+      <div className="flex gap-2 relative    justify-between mx-auto max-w-5xl includes 3 component">
+        <SideNavigation />
+        <div className="max-w-xl mx-auto w-full border-x border-[#38444d] ">
+          <FeedHeader />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+        </div>
+        <div className=" lg:block hidden w-80 border-x border-[#38444d] ">
+          <div className="sticky top-2 text-white ">RightBar</div>
+        </div>
       </div>
     </div>
   );
