dc8aea54faa5c45f7054613e7b5fca3ffd99753d
erennilufer <eren.nilufer@outlook.com>
2022-09-20 03:43:26 +0300

    created TweetModel

---
+++ client/src/models/TweetModel.ts
@@ -0,0 +1,10 @@
+export interface TweetModel {
+  authorName: string;
+  comments: any[];
+  createdAt: string;
+  likes: number;
+  retweets: number;
+  textContent: string;
+  updatedAt: string;
+  _id: string;
+}
