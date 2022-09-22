6a8dff6375d07a4a241a71255f8a5bde016a32b0
erennilufer <eren.nilufer@outlook.com>
2022-09-22 03:05:29 +0300

    added loader

--- client/src/components/Feed/Feed.tsx
+++ client/src/components/Feed/Feed.tsx
@@ -1,3 +1,4 @@
+import { CogIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 import { getTweets } from "../../api/tweet";
 import { TweetModel } from "../../models/TweetModel";
@@ -6,14 +7,28 @@ import Tweet from "../Tweet/Tweet";
 interface Props {}
 
 const Feed = (props: Props) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [tweets, setTweets] = useState<TweetModel[]>();
   useEffect(() => {
-    getTweets().then((data) => setTweets(data));
+    setIsLoading(true);
+    getTweets().then((data) => {
+      setTweets(data.data);
+      setIsLoading(false);
+    });
   }, []);
   return (
-    <div>
-      {tweets && tweets.map((tweet: TweetModel) => <Tweet tweet={tweet} />)}
-    </div>
+    <>
+      {isLoading ? (
+        <CogIcon className="mt-3 w-8 h-8   mx-auto  text-[#8B98A5] animate-spin" />
+      ) : (
+        <div>
+          {tweets &&
+            tweets.map((tweet: TweetModel) => (
+              <Tweet key={tweet._id} tweet={tweet} />
+            ))}
+        </div>
+      )}
+    </>
   );
 };
 
